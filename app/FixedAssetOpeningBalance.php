b71c21c414c1ff9a19e5048f964a6bffd8a7652e
Ahmed Salah <asalahdev5@gmail.com>
2025-09-02 14:44:53 +0300

    d

--- app/FixedAssetOpeningBalance.php
+++ app/FixedAssetOpeningBalance.php
@@ -22,17 +22,17 @@ class FixedAssetOpeningBalance extends Model
 	{
 		return '';
 	}
-		public static function booted()
-	{
-			parent::boot();
-			static::saving(function(self $model){
-				dd($model);
-				$openingBalance = $model->{self::getOpeningBalanceColumnName()};
-				$statementPayload = $model->{self::getPayloadStatementColumn()};
-				$dateIndexWithDate = $model->project->getDateIndexWithDatE();
-				$model->statement = self::calculateSettlementStatement($statementPayload,[],$openingBalance,$dateIndexWithDate);
-			});
-	}
+	// 	public static function booted()
+	// {
+	// 		parent::boot();
+	// 		static::saving(function(self $model){
+	// 			dd($model);
+	// 			$openingBalance = $model->{self::getOpeningBalanceColumnName()};
+	// 			$statementPayload = $model->{self::getPayloadStatementColumn()};
+	// 			$dateIndexWithDate = $model->project->getDateIndexWithDatE();
+	// 			$model->statement = self::calculateSettlementStatement($statementPayload,[],$openingBalance,$dateIndexWithDate);
+	// 		});
+	// }
 	
     public function project():BelongsTo
     {
